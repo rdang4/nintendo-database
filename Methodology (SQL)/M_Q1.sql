b500ca3a6064380d60e055a92e5dc94aefa1b8e5
Ryan Dang <145620863+rdang4@users.noreply.github.com>
2024-02-08 17:28:51 -0800

    Update M_Q1.sql

--- Methodology (SQL)/M_Q1.sql
+++ Methodology (SQL)/M_Q1.sql
@@ -1,1 +1,7 @@
+/***********************************************
 
+This will be Question 1 from Methodology (SQL)
+
+NINTENDO SWITCH LIBRARY AND DATA ANALYTICS 
+
+************************************************/
