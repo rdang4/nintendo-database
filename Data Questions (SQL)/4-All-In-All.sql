b38b999ef014617a35f4fa450e89e6be43d2f764
Ryan Dang <145620863+rdang4@users.noreply.github.com>
2024-03-04 19:54:46 -0800

    Update and rename 4-All-In-All to 4-All-In-All.sql

---
+++ Data Questions (SQL)/4-All-In-All.sql
@@ -0,0 +1,39 @@
+/*****************************************
+
+NINTENDO SWITCH LIBRARY AND DATA ANALYTICS 
+
+******************************************
+ALL IN ALL
+******************************************/
+
+DROP TABLE IF EXISTS complete_joint_dataset;
+CREATE TEMP TABLE complete_joint_dataset AS
+SELECT 
+       t1.genre_id,
+       t1.genre_name,
+       avg_score, 
+       game_count,
+       percentage_genres
+
+FROM genre_count_and_avg AS t1
+
+INNER JOIN percent_genre_joint_table AS t2
+	ON t1.genre_id = t2.genre_id;
+
+SELECT * FROM complete_joint_dataset
+ORDER BY percentage_genres DESC
+LIMIT 10;
++──────────+────────────────────+───────────+────────────+───────────────────+
+| genre_id | genre_name         | avg_score | game_count | percentage_genres |
++──────────+────────────────────+───────────+────────────+───────────────────+
+| 22      | Platformer          | 86.30	    | 10	       | 14.08             |
+| 24      | RPG          	      | 80.29	    | 7          | 9.86	             |
+| 19      | Open-World          | 86.67	    | 6          | 8.45	             |
+| 2       | Action Adventure    | 86.40     | 5          | 7.04              |
+| 41      | JRPG	              | 84.00     | 5          | 7.04	             |
+| 4       | Action RPG          | 86.60	    | 5          | 7.04	             |
+| 3       | Action Puzzle       | 86.60	    | 3          | 4.23	             |
+| 23      | Puzzle              | 86.60	    | 3          | 4.23	             |
+| 14      | Fighting            | 86.60	    | 3          | 4.23	             |
+| 20      | Party/Minigame      | 86.60	    | 3          | 4.23	             |
++──────────+────────────────────+───────────+────────────+───────────────────+
